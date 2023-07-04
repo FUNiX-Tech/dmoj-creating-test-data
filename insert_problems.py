e1d4fe8762eeb6971b93a7de1ff880d17ac4dd78
nguyen-the-anh-vu <vuntavfx17889@funix.edu.vn>
2023-07-04 16:21:46 +0700

    fixed insert_problems.py

--- insert_problems.py
+++ insert_problems.py
@@ -1,11 +1,11 @@
 template_problem_code = 'hungry'
-cloned_problem_code_prefix = 'clonehungry'
+cloned_problem_code_prefix = 'v1clonehungry'
 cloned_problem_name_prefix = 'Clone Problem '
 cloned_problems_quantity = 2000
 
 from init_db import db
 cursor = db.cursor()
-sql = 'insert into judge_problem values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
+sql = 'insert into judge_problem values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
     
 # get template problem
 cursor.execute("select * from judge_problem where code = '{}'".format(template_problem_code))
@@ -15,7 +15,7 @@ i = 1
 while i <= cloned_problems_quantity:
     cloned_code = cloned_problem_code_prefix + str(i)
     cloned_name = cloned_problem_name_prefix + str(i)
-    cloned_values = (cloned_code, cloned_name) +  template_problem_values[3:]
+    cloned_values = (None, cloned_code, cloned_name) +  template_problem_values[3:]
     cursor.execute(sql, cloned_values)
     db.commit()
     print('success {}'.format(i))
